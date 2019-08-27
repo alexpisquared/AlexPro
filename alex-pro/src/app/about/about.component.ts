845cd0085ce44f203caacb42825e81c064c09439
AlexPi <alex.pigida@outlook.com>
2019-08-27 10:04:29 -0400

    formatting changes only

--- alex-pro/src/app/about/about.component.ts
+++ alex-pro/src/app/about/about.component.ts
@@ -9,16 +9,14 @@ import { Location } from '@angular/common';
 export class AboutComponent implements OnInit {
   result = 'Result : nothing ...yet';
 
-  constructor(private location: Location) { }
+  constructor(private location: Location) {}
 
   ngOnInit() {
     this.getRandom();
   }
 
   getRandom() {
-    this.result = `There is a number between 0 and 100000000, and believe it or not but it is   ${Math.floor(
-      Math.random() * 100000000
-    )}`;
+    this.result = `There is a number between 0 and 100000000, and believe it or not but it is   ${Math.floor(Math.random() * 100000000)}`;
   }
 
   goBack(): void {
